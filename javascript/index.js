39e52be244bd48c911be6e443ef26f95e1a54578
Eva Tavares <eva.programadora@gmail.com>
2023-02-24 21:34:07 -0300

    Add - Funções abrir e fechar

--- javascript/index.js
+++ javascript/index.js
@@ -8,3 +8,18 @@ window.addEventListener('load', function(){
           next: '.glider-next'
         }
       })});
+
+const menu = document.querySelector(".bar-menu-mob");
+
+menu.addEventListener("click", function() {
+  const open = document.getElementById("menu-mobile");
+  open.setAttribute("style", "left:0")
+});
+
+const closed = document.querySelector(".close-menu-mob");
+
+closed.addEventListener("click", function(){
+  const close = document.getElementById("menu-mobile");
+  close.setAttribute("style", "left:-100%")  
+})
+
