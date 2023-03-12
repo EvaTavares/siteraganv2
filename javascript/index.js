120e4e22b2d5c54b9a7e2b0f8f347fb592c98a63
Eva Tavares <eva.programadora@gmail.com>
2023-03-12 20:24:38 -0300

    Responsivo-carrosel e marketing

--- javascript/index.js
+++ javascript/index.js
@@ -6,7 +6,30 @@ window.addEventListener('load', function(){
         arrows: {
           prev: '.glider-prev',
           next: '.glider-next'
-        }
+        },
+        responsive: [
+          {
+            breakpoint: 1,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+            }
+          },
+          {
+            breakpoint: 768,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 1,
+            }
+          },
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 1,
+            }
+          }
+        ]
       })});
 
 const menu = document.querySelector(".bar-menu-mob");
